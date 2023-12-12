748eaa59bb077ebbb3112ee694cc5051541a8ebc
GabrielaMat <gabriela.ayumi@epiousion.com.br>
2023-12-12 06:56:32 -0300

    change babel

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  presets: [
-    ['@vue/app', {
-      polyfills: [
-        'es.promise',
-        'es.symbol'
-      ]
-    }]
-  ]
+module.exports = { 
+  presets: [ 
+    [ 
+      "@ vue / app", 
+        { 
+          useBuiltIns: "entry" 
+        } 
+    ] 
+  ] 
 }
