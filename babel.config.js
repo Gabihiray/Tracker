03de1e06fbfdc07b93398f671c8184fb750d4769
GabrielaMat <gabriela.ayumi@epiousion.com.br>
2023-12-12 06:57:39 -0300

    change babel

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,5 @@
-module.exports = { 
-  presets: [ 
-    [ 
-      "@ vue / app", 
-        { 
-          useBuiltIns: "entry" 
-        } 
-    ] 
-  ] 
+module.exports = {
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
 }
